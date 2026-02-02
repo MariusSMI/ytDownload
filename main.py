c780978ceca5747bd53ae3b797855d8ff8d8d147
MariusSMI <smaliimariusilie@proton.me>
2026-02-03 00:56:58 +0100

    GUI done

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from tkinter import *
 
 root = Tk()
 root.title("ytDonwload")
-root.geometry("420x540")
+root.geometry("500x300")
 
 # Widgets and Frames
 
@@ -30,15 +30,16 @@ link_entry = Entry(link_frame).grid(row=0, column=1, padx=20, pady=20)
 format_frame = Frame(root)
 format_frame.pack()
 
-format_mp4 = Button(format_frame, text=f"MP4", background="red").grid(row=0, column=0, padx=20, pady=20)
-format_mp3 = Button(format_frame).grid(row=0, column=1, padx=20, pady=20)
-format_convert = Button(format_frame).grid(row=0, column=2, padx=20, pady=20)
-format_single = Button(format_frame).grid(row=0, column=3, padx=20, pady=20)
-format_playlist = Button(format_frame).grid(row=0, column=4, padx=20, pady=20)
+format_mp4 = Button(format_frame, text=f"MP4").grid(row=0, column=0, padx=10, pady=20)
+format_mp3 = Button(format_frame, text=f"MP3").grid(row=0, column=1, padx=10, pady=20)
+format_convert = Button(format_frame, text=f"Start", background="green").grid(row=0, column=2, padx=10, pady=20)
+format_single = Button(format_frame, text=f"Single").grid(row=0, column=3, padx=10, pady=20)
+format_playlist = Button(format_frame, text=f"List").grid(row=0, column=4, padx=10, pady=20)
 
 status_frame = Frame(root)
 status_frame.pack()
 
+status_text = Label(status_frame, text=f"Waiting...").grid(row=0, column=0, padx=20, pady=20)
 
 
 root.mainloop()
