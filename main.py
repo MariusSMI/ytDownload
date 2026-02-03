29ec04e810c93a20307f9a36618da916c684b6c6
MariusSMI <smaliimariusilie@proton.me>
2026-02-03 02:55:40 +0100

    1.0.0

--- main.py
+++ main.py
@@ -1,45 +1,93 @@
 from tkinter import *
+from tkinter import filedialog
+import subprocess
 
 # Global variables
 
-
+path = ""
+mp4 = True
+mp3 = False
 
 # Functions
 
+def askDirectory():
+    global path
+    path = filedialog.askdirectory()
+    folder_stat["text"]=path
 
+def convert(link):
+    global path, mp4, mp3
+    if link != "":
+        status_text["text"]="Everything is good!"
+        if mp4:
+            subprocess.run(["yt-dlp", "-P", path, "-f" "best[height<=1080]", link])
+        if mp3:
+            subprocess.run(["yt-dlp", "-P", path , "-x", "--audio-format", "mp3", link])
+    else:
+        status_text["text"]="Error. Put a valid link"
+
+def changeToMp4():
+    global mp4, mp3
+    mp4 = True
+    mp3 = False
+    if mp4:
+        format_mp4["background"]="#273c75"
+        format_mp3["background"]="#2f3640"
+
+def changeToMp3():
+    global mp4, mp3
+    mp4 = False
+    mp3 = True
+    if mp3:
+        format_mp4["background"]="#2f3640"
+        format_mp3["background"]="#273c75"
 
 root = Tk()
 root.title("ytDonwload")
 root.geometry("500x300")
+root.resizable(False, False)
+root["background"]="#192a56"
 
 # Widgets and Frames
 
+# 1
 folder_frame = Frame(root)
 folder_frame.pack()
+folder_frame["background"]="#192a56"
 
-folder_text = Label(folder_frame, text=f"Directory:").grid(row=0, column=0, padx=20, pady=20)
-folder_stat = Label(folder_frame, text=f"Null").grid(row=0, column=1, padx=20, pady=20)
-folder_btn = Button(folder_frame, text=f"Change").grid(row=0, column=2, padx=20, pady=20)
+folder_btn = Button(folder_frame, text=f"Folder", command=askDirectory)
+folder_btn.grid(row=0, column=1, padx=20, pady=20)
+folder_stat = Label(folder_frame, text="None")
+folder_stat.grid(row=0, column=2, padx=20, pady=20)
 
+# 2
 link_frame = Frame(root)
 link_frame.pack()
+link_frame["background"]="#192a56"
 
-link_text = Label(link_frame, text=f"Link: ").grid(row=0, column=0, padx=20, pady=20)
-link_entry = Entry(link_frame).grid(row=0, column=1, padx=20, pady=20)
+link_text = Label(link_frame, text=f"Link: ")
+link_text.grid(row=0, column=0, padx=20, pady=20)
+link_entry = Entry(link_frame)
+link_entry.grid(row=0, column=1, padx=20, pady=20)
 
+# 3
 format_frame = Frame(root)
 format_frame.pack()
+format_frame["background"]="#192a56"
 
-format_mp4 = Button(format_frame, text=f"MP4").grid(row=0, column=0, padx=10, pady=20)
-format_mp3 = Button(format_frame, text=f"MP3").grid(row=0, column=1, padx=10, pady=20)
-format_convert = Button(format_frame, text=f"Start", background="green").grid(row=0, column=2, padx=10, pady=20)
-format_single = Button(format_frame, text=f"Single").grid(row=0, column=3, padx=10, pady=20)
-format_playlist = Button(format_frame, text=f"List").grid(row=0, column=4, padx=10, pady=20)
+format_mp4 = Button(format_frame, text=f"MP4", command=changeToMp4, background="#273c75")
+format_mp4.grid(row=0, column=0, padx=10, pady=20)
+format_mp3 = Button(format_frame, text=f"MP3", command=changeToMp3, background="#2f3640")
+format_mp3.grid(row=0, column=1, padx=10, pady=20)
+format_convert = Button(format_frame, text=f"Start", background="#4cd137", command=lambda: convert(link_entry.get()))
+format_convert.grid(row=0, column=2, padx=10, pady=20)
 
+# 4
 status_frame = Frame(root)
 status_frame.pack()
+status_frame["background"]="#192a56"
 
-status_text = Label(status_frame, text=f"Waiting...").grid(row=0, column=0, padx=20, pady=20)
-
+status_text = Label(status_frame, text=f"Everything is good!")
+status_text.grid(row=0, column=0, padx=20, pady=20)
 
 root.mainloop()
